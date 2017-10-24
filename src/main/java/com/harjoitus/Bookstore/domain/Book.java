d09fd9ca2f9710fc378d8f549be435925065e47f
NeonDemon88 <a1500957@myy.haaga-helia.fi>
2017-10-24 10:56:02 +0300

    adding new book kinda works now

--- src/main/java/com/harjoitus/Bookstore/domain/Book.java
+++ src/main/java/com/harjoitus/Bookstore/domain/Book.java
@@ -4,9 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
@@ -19,8 +16,6 @@ public class Book {
     private String isbn;
     private double price;
 
-   
-    @JsonIgnore
 
     public Book() {}
 
